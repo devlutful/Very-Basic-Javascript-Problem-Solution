30f3f9e6c8444034ef06ffee16043f87f70f5205
Lutful Islam Ahmed <74391615+devlutful@users.noreply.github.com>
2020-11-24 12:09:51 +0530

    Add files via upload

---
+++ Day7.js
@@ -0,0 +1,51 @@
+//100Days of code-Day 6 - 
+//Auhtor-  Lutful
+
+
+
+//Is Even return true else return false with arrow function
+//Solution
+isEven=(n)=>{
+	if(n%2===0){
+		return true 
+	}else {
+		return false
+	}
+}
+// console.log(isEven(20, 23))
+
+
+//Arry Reversing
+//SOlution
+arrReverse=(a)=>{
+	return a.reverse()
+}
+// console.log(arrReverse([2,3,4,5,6,78,-1]))
+
+
+//Word without First Character
+newWord=(str)=>{
+	return str.slice(1)
+}
+// console.log(newWord("Apple"))
+
+
+//Evaluate an Equation
+eq=(evaluate)=> {
+		return eval(evaluate)
+}
+// console.log(eq("2+2+(2-2)"))
+
+
+
+//Greetings
+const helloName = name => " Hello " + name
+// console.log(helloName("Lutful "))
+
+
+//On/Off Switches
+//solution
+const posCom= num=>Math.pow(2, num)
+// console.log(posCom(10))
+
+
